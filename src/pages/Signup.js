d0a0a638d26979390618be1997f61f321a713a88
kartikmaurya18 <kartikmaurya18@gmail.com>
2024-11-05 13:30:54 +0530

    hello

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import './signup.css';
 
 function Signup() {
   const [email, setEmail] = useState('');
